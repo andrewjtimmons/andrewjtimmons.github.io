b99e8200e239ec7e6052e01190f4a5552c098417
andrew timmons <andrewjtimmons@andrews-MacBook-Air.local>
2014-09-25 00:30:17 -0400

    tell mobile to get on a desktop

--- squaresAndCircles/script.js
+++ squaresAndCircles/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   var i = 1;
 
   if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          document.write("<h1>please<br/>look<br/>at<br/>this<br/>on<br/>a<br/>desktop<br/>computer</h1>")
+          document.write("<h2>please<br/>look<br/>at<br/>this<br/>on<br/>a<br/>desktop<br/>computer</h2>")
         }
   else {
     initialize();
