3c05fed2d65c55d19cfba3a0399a01c6f2b1390c
andrew timmons <andrewjtimmons@andrews-MacBook-Air.local>
2014-09-24 23:48:30 -0400

    squares and circles fix for tweet 2

--- squaresAndCircles/script.js
+++ squaresAndCircles/script.js
@@ -5,58 +5,42 @@ $(document).ready(function() {
   var numPhotos = 24
   var images = createImages();
   var i = 1;
-  //var imageObj = new Image;
 
   initialize();
 
+  setInterval(function() {
+    redraw()
+  }, 472);
+
   function initialize() {
+    //create the universe
     window.addEventListener('resize', resizeCanvas, false);
     resizeCanvas();
   }
 
   function createImages() {
+    //creates array of images for redraw to loop thru
     var images = []
     for (var i = 1; i <= numPhotos; i++) {
       images[i] = new Image;
-      //images[i].src = "img/" + i + ".JPG";
       $(images[i]).load().attr('src', "img/" + i + ".JPG");
     }
     return images;
   }
 
-  // Runs each time the DOM window resize event fires.
-  // Resets the canvas dimensions to match window,
-  // then draws the new borders accordingly.
   function resizeCanvas() {
     htmlCanvas.width = window.innerWidth;
     htmlCanvas.height = window.innerHeight;
     htmlCanvas.style.zIndex="-1";
-    tweet.width = window.innerWidth;
-    tweet.height = window.innerHeight;
-    tweet.style.zIndex="-1";
   }
-  
-  setInterval(function() {
-    redraw()
-  }, 472);
-  // Display custom canvas.
-  // In this case it's a blue, 5 pixel border that 
-  // resizes along with the browser window.                    
+             
   function redraw() {
-    //imageObj.src = "img/" + photoID + ".JPG";
-    // imageObj.onload = function() {
-    //   context.drawImage(images[1],0,0);
-    // }
+    //loops through var images and displays them to user
     if (i == 24) {
       i = 1;
     }
     context.drawImage(images[i],0,0, 1000*(window.innerWidth/1000),1000*(window.innerHeight/1000) );
     i++;
-    // context.strokeStyle = 'blue';
-    // context.lineWidth = '5';
-    // context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
   }
-
-
 
 });
